5f9adc69c81260d02320af75d5e600c94f158862
Fenton Travers <fenton.travers@gmail.com>
2018-08-16 14:13:40 -0700

    get recom dependencies loaded, fix core async error

--- src/cljs/sample_reagent/core.cljs
+++ src/cljs/sample_reagent/core.cljs
@@ -1,47 +1,9 @@
 (ns sample-reagent.core
   (:require
-   [reagent.core :as reagent]
-   ;; [rum.core :as rum]
-   ))
-
-;; (rum/defc label [text]
-;;   [:div {:class "label"} text])
-
-(def person (reagent/atom "Rod"))
-
-(comment
-  (reset! person "blah") 
-  )
-
-
-(defn get-name []
-  (->> "name-box"
-       (.getElementById js/document)
-       .-value))
-
-(defn change-name-button []
-  [:button
-   {:on-click #(reset! person (get-name))}
-   "Change Name!"])
-
-(defn input-box []
-  [:input {:id "name-box"}])
+   [reagent.core :as reagent]))
 
 (defn body []
-  [:div
-   [input-box]
-   [:br]
-   @person
-   [:br]
-   [change-name-button]])
-
-;; (defn mount-rum []
-;;   (rum/mount (label "abc123") js/document.body))
-
-(defn mount-reagent []
-  (reagent/render
-   [body]
-   (.getElementById js/document "app")))
+  [:div "hello"])
 
 (defn on-js-reload []
   (reagent/render
